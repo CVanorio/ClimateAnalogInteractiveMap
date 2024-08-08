1daea3eeae831bd9385ec9171a0d34beaf6ed7df
Courtney Vanorio <courtney.vanorio@gmail.com>
2024-08-08 11:09:06 -0500

    converts db connection to pull mockDB files

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,23 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3000';
-
 export const fetchData = async (selectedCounty, timeScale, targetYear, scaleValue, selectedDataType) => {
   try {
-    //console.log("Inside API.js")
-    const response = await axios.get(`${BASE_URL}/getData`, {
-      params: {
-        targetCounty: selectedCounty,
-        timeScale: timeScale,
-        timeScaleValue: scaleValue,
-        year: targetYear,
-        dataType: selectedDataType,
-      },
-    });
-   // console.log('API response:', response.data);
-    return response.data;
+    // Construct the file name
+    const fileName = `${selectedCounty}_${timeScale}_${targetYear}_${scaleValue || ''}_${selectedDataType}.json`;
+    
+    // Fetch the file from the server
+    const response = await fetch(`./mockDB/${fileName}`);
+    
+    // Check if the response is ok
+    if (!response.ok) {
+      throw new Error(`Error fetching data: ${response.statusText}`);
+    }
+
+    // Parse the JSON content
+    const data = await response.json();
+
+    // Return the data
+    return data;
   } catch (error) {
     console.error('Error fetching data:', error);
     return error;
