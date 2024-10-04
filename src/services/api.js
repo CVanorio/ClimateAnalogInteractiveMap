ca1892edf2ed45c6940fd73a4ed82a3ea4572df6
Courtney Vanorio <courtney.vanorio@gmail.com>
2024-10-04 07:43:27 -0500

    updates marker border color and updates API base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3000'; //https://wwwtest.climatology.nelson.wisc.edu/climate-change-app/
+const BASE_URL = 'https://apps.nelson.wisc.edu/climate-change'
 
 // Function to fetch data from the server based on user selections
 export const fetchData = async (selectedCounty, timeScale, targetYear, scaleValue, selectedDataType) => {
